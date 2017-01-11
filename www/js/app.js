d0b83c82b39fd277b279f20cc097affa7c4c6901
bcochran <b.cochrane@imperial.ac.uk>
2017-01-11 19:50:51 +0000

    app.js remove boilerplate view

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic',])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -28,8 +28,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     .state('app', {
     url: '/app',
     abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
+    templateUrl: 'templates/menu.html'
   })
 
   .state('app.profile', {
