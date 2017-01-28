9641bd938feb3cb230d494a40aeb153bfc747903
somabc <somabc@gmail.com>
2017-01-28 14:51:55 +0000

    database structure

--- www/js/controllers/auth.js
+++ www/js/controllers/auth.js
@@ -1,19 +1,19 @@
 'use strict';
 
-app.controller('AuthCtrl', function(Auth) {
+app.controller('AuthCtrl', function(Auth, $state) {
 
     var auth = this;
 
     auth.login = function() {
         console.log('Login clicked');
-        
-        return Auth.login().then(function(user) {
-            console.log(result.user);
+
+        return Auth.login().then(function(result) {
+            $state.go('app.home');
         });
-        
+
     };
 
     auth.logout = function() {
         Auth.logout();
     };
-});
+});ß
