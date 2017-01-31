71369918166387be31a60e95d919a212add3e00b
bcochran <b.cochrane@imperial.ac.uk>
2017-01-31 20:10:52 +0000

    matching

--- www/js/services/match.js
+++ www/js/services/match.js
@@ -1,38 +1,33 @@
 'use strict';
 
-app.factory('Match', function(FURL, $firebaseArray, $ionicPopup) {
+app.factory('Match', function($firebaseArray, $ionicPopup) {
 
-	var ref = new Firebase(FURL);
+    var ref = firebase.database().ref();
 
-	var Match = {
+    var Match = {
 
-		allMatchesByUser: function(uid) {
-			return $firebaseArray(ref.child('matches').child(uid));
-		},
+        allMatchesByUser: function(uid) {
+            return $firebaseArray(ref.child('matches').child(uid));
+        },
 
-		checkMatch: function(uid1, uid2) {
-			var check = ref.child('likes').child(uid2).child(uid1);
+        checkMatch: function(uid1, uid2) {
+            var check = ref.child('likes').child(uid2).child(uid1);
 
-			check.on('value', function(snap) {
-				if (snap.val() != null) {
-					ref.child('matches').child(uid1).child(uid2).set(true);
-					ref.child('matches').child(uid2).child(uid1).set(true);
+            check.on('value', function(snap) {
+                if (snap.val() != null) {
+                    ref.child('matches').child(uid1).child(uid2).set(true);
+                    ref.child('matches').child(uid2).child(uid1).set(true);
 
-					$ionicPopup.alert({
-						title: 'Matched',
-						template: 'Yay, you guys are matched!'
-					});
-				}
-			})
-		},
+                    $ionicPopup.alert({
+                        title: 'Matched',
+                        template: 'Yay, you guys are matched!'
+                    });
+                }
+            })
+        }
 
-		removeMatch: function(uid1, uid2) {
-			ref.child('matches').child(uid1).child(uid2).remove();
-			ref.child('matches').child(uid2).child(uid1).remove();
-		}
+    };
 
-	};
-
-	return Match;
+    return Match;
 
 });
