91b15816fbbb0dcd738261759c0fbada107f369a
bcochran <b.cochrane@imperial.ac.uk>
2017-01-31 17:01:07 +0000

    FB graph API

--- www/js/services/auth.js
+++ www/js/services/auth.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.factory('Auth', function($firebaseAuth, $firebaseObject, $state) {
+app.factory('Auth', function($firebaseAuth, $firebaseObject, $state, $http, $q) {
     var ref = firebase.database().ref();
     var auth = $firebaseAuth();
 
@@ -19,7 +19,6 @@ app.factory('Auth', function($firebaseAuth, $firebaseObject, $state) {
             provider.addScope('public_profile, email, user_location, user_birthday, user_photos, user_about_me');
 
             return auth.$signInWithPopup(provider)
-
                 .then(function(result) {
                     var accessToken = result.credential.accessToken;
                     var user = Auth.getProfile(result.user.uid).$loaded();
@@ -27,13 +26,28 @@ app.factory('Auth', function($firebaseAuth, $firebaseObject, $state) {
                     user.then(function(profile) {
                         if (profile.name == undefined) {
 
-                            var info = result.user.providerData[0];
-                            var profile = {
-                                name: info.displayName,
-                                email: info.email,
-                                avatar: info.photoURL,
-                            }
-                            Auth.createProfile(result.user.uid, profile);
+                            var genderPromise = $http.get('https://graph.facebook.com/me?fields=gender&access_token=' + accessToken);
+                            var birthdayPromise = $http.get('https://graph.facebook.com/me?fields=birthday&access_token=' + accessToken);
+                            var locationPromise = $http.get('https://graph.facebook.com/me?fields=location&access_token=' + accessToken);
+                            var bioPromise = $http.get('https://graph.facebook.com/me?fields=about&access_token=' + accessToken);
+                            var imagesPromise = $http.get('https://graph.facebook.com/me/photos/uploaded?fields=source&access_token=' + accessToken);
+                            var promises = [genderPromise, birthdayPromise, locationPromise, bioPromise, imagesPromise];
+
+                            $q.all(promises).then(function(data) {
+                                var info = result.user.providerData[0];
+                                var profile = {
+                                    name: info.displayName,
+                                    email: info.email,
+                                    avatar: info.photoURL,
+                                    gender: data[0].data.gender ? data[0].data.gender : "",
+                                    birthday: data[1].data.birthday ? data[1].data.birthday : "",
+                                    age: data[1].data.birthday ? Auth.getAge(data[1].data.birthday) : "",
+                                    location: data[2].data.location ?  data[2].data.location.name : "",
+                                    bio: data[3].data.about ? data[3].data.about : "",
+                                    images: data[4].data.data
+                                }
+                                Auth.createProfile(result.user.uid, profile);
+                            });
                         }
                     });
                 });
